acf0d0bdf1c5d74b5f38a44a4375124451be59be
Djordje Knezevic <starwarrior@mycity.rs>
2022-10-06 16:49:50 +0200

    Fix progress bar bug

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -14,19 +14,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     
     let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
-        
+    
     var timer = Timer()
     
     var totalTime = 0
     var secondsPassed = 0
-
+    
     @IBAction func hardnessSelected(_ sender: UIButton) {
-                
+        
         timer.invalidate()
         
         let hardness = sender.currentTitle!
         
         totalTime = eggTimes[hardness]!
+        
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
         
@@ -34,10 +38,8 @@ class ViewController: UIViewController {
     
     @objc func updateTimer() {
         if secondsPassed < totalTime {
-            let percentageProgress = secondsPassed / totalTime
-            
-            progressBar.progress = Float(percentageProgress)
             secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
         } else {
             timer.invalidate()
             titleLabel.text = "Done!"
