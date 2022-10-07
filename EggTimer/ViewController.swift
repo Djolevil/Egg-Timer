fd4bfdfa437ee4d18c06627738e7e28d2eed6c1c
Djordje Knezevic <starwarrior@mycity.rs>
2022-10-07 12:14:06 +0200

    Update app to play alarm sound when timer is done

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
@@ -16,7 +17,7 @@ class ViewController: UIViewController {
     let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
     
     var timer = Timer()
-    
+    var player: AVAudioPlayer!
     var totalTime = 0
     var secondsPassed = 0
     
@@ -43,6 +44,11 @@ class ViewController: UIViewController {
         } else {
             timer.invalidate()
             titleLabel.text = "Done!"
+            
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+            
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
         }
     }
     
